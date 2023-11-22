f2f7536217c2222fc57bcd34b84b755d00d17c92
CarlosEduFF <Carlos.farias11@etec.sp.gov.br>
2023-11-22 14:36:16 -0300

    App development using SQLite and ROOM

---
+++ app/src/main/java/com/example/appnovotecroom/model/Contato.kt
@@ -0,0 +1,13 @@
+package com.example.appnovotecromm.model
+
+import com.example.appnovotecroom.controller.Tipos
+
+//Classe para instanciar cada atributo
+data class Contato(
+    val contato: List<Contato> = emptyList(),
+    val nome: String = "",
+    val sobrenome: String = "",
+    val telefome: String = "",
+    val adicionarContato: Boolean = false,
+    val tipos: Tipos = Tipos.nome
+)
